b4f4dd5942aea6d2a2302f316850c594d8f7072f
Matthew Doerr <MatthewRyanDoerr@gmail.com>
2016-04-05 21:59:41 -0500

    more db changes

--- models/database.php
+++ models/database.php
@@ -8,9 +8,12 @@ class DatabaseObject {
 	//protected static $database_db;
 	public $database_db;
 	//public static $database;
-	public $field_set; //array that holds table field associative array
+	protected $field_set; //array that holds table field associative array
+	protected $my_field_set;//holds any necessary appends to the array
 	protected $query; //actual query for reciept by medoo
+	protected $joinQuery;
 	protected $data_set; //returned data set
+	protected $join = false; //boolean for innerJoin
 	protected $table;
 	protected $columns;
 	protected $where;
@@ -21,6 +24,10 @@ class DatabaseObject {
 	public function getRecords($columns, $where = "") {
 		$this->columns = $columns;
 		if (!empty($where)){
+				if ($this->join){
+					$this->where = $where;
+					$this->data_set = $this->database_db->select($this->table, $this->join, $columns, $this->where);
+				}
 			$this->where = $where;
 			$this->data_set = $this->database_db->select($this->table, $columns, $this->where);
 		}
@@ -33,6 +40,19 @@ class DatabaseObject {
 		$this->lastInstertId = $database_db->insert($table, $data);
 	}
 	public function display($howMany) {
+	}
+	//$switch is boolean, turns inner join on.
+
+	//$stageDBO->setJoin("order_details", "custom_order");
+	// The row author_id from table post is equal the row user_id from table account
+	//"[>]account" => ["author_id" => "user_id"],
+	public function setJoin($joinTable, $joinColumn){
+		$this->join = true;
+		$this->joinQuery = array($joinTable, $joinColumn, $joinTable.$joinColumn);
+
+
+		//('' => , );'"[><]".$joinTable' => ['$joinColumn', '$joinTable.$joinColumn'];
+		//$this->joinQuery = '"[><]'.$joinTable.'" => ["'.$joinColumn.'" => "'.$joinTable.'.'.$joinColumn'"]';
 	}
 	public function set_table($string){
 		$this->table = $string;
@@ -70,7 +90,6 @@ class OrderDatabaseObject extends DatabaseObject {
 
 	function __construct(){
 		parent::__construct();
-		//echo 'constructor called';
 		$this->field_set = array( 'order_id', 'customer_id', 'employee_id', 'order_date',
 					   'subtotal', 'tax_amount', 'total_price', 'order_type' );
 		$this->set_table('order');
@@ -78,18 +97,40 @@ class OrderDatabaseObject extends DatabaseObject {
 	//function getRecords
 
 }
-class Order_DetailsDatabaseObject extends orderDatabaseObject {
-	private $my_field_set = array('order_id','item_id', 'item_price', 'qty');
+class OrderDetailsDatabaseObject extends OrderDatabaseObject {
+	
 	function __construct(){
 		parent::__construct();
-		array_push($field_set, $my_field_set); 
+		$this->my_field_set = array('order_id','item_id', 'item_price', 'qty');
 		$this->set_table('order_details');
 	}
 }
+class GiftOrderDatabaseObject extends OrderDatabaseObject {
+	function __construct(){
+		parent::__construct();
+		$this->my_field_set = array('gift_id','order_id','rec_last_name','rec_first_name','address_id');
+		$this->set_table('gift_order');
+
+	}
+}
+//base object is no my_field_set
+class AddressDatabaseObject extends DatabaseObject {
+	function __construct(){
+		parent::__construct();
+		$this->field_set = array('address_id', 'street_number', 'street_suffix', 'street_name',
+								 'street_type','street_type','street_direction','address_type',
+								 'address_type_identifier','minor_municipality','major_municipality',
+								 'governing_district','zip', 'iso_country_code');
+		$this->set_table('address');
+
+	}
+}
+
 
 class DatabaseObjectFactory {
 	public static function build($tableName) {
-		$DatabaseObject = ucwords($tableName) . "DatabaseObject";
+		$tableName = ucwords(str_replace("_", " ", $tableName));
+		$DatabaseObject = str_replace(" ","", $tableName."DatabaseObject");
 		//var_dump($DatabaseObject);
 		if (class_exists($DatabaseObject)) {
 			return new $DatabaseObject;
--- views/pages/stage.php
+++ views/pages/stage.php
@@ -1,9 +1,15 @@
 <?php
 	$stageDBO = DatabaseObjectFactory::build('order');
-	$arr = ['order_id', 'customer_id', 'employee_id'];
-	$filter = ['order_id' => '6001'];
-	$records = $stageDBO->getRecords($arr, $filter);
+	$arr = ['customer_id', 'subtotal'];
+	$joinTable = "customer";
+	$joinColumn = "customer_id";
+	//$filter = ['order_id[>]' => '6013'];
+	//$stageDBO->setJoin(["[><]order_details" => ["order_id" => "custom_order.order_id"]]);
+	$stageDBO->setJoin($joinTable, $joinColumn);
+	$records = $stageDBO->getRecords($arr);
 	$stageDBO->drawTable();
+	// The row author_id from table post is equal the row user_id from table account
+	//"[>]account" => ["author_id" => "user_id"],
 	//var_dump($records);
 	//var_dump($stageDBO->$field_set);
 ?>
