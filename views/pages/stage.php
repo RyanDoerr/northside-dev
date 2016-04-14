ccf4fc57e95df9efd515008ba4100fddfeef290e
Alex Hornick <tennalexd@yahoo.com>
2016-04-14 10:42:28 -0500

    Stuff

--- views/pages/stage.php
+++ views/pages/stage.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 	$stageDBO = DatabaseObjectFactory::build('order');
 	$arr = ['gift_id', 'order_id'];
 	//$stageDBO->join();
@@ -7,13 +6,11 @@
 	//["[><]gift_order" => "order_id", "[><]custom_order" => "order_id", "[><]address" => "address_id"]
 	$records = $stageDBO->getRecords($arr);
 	$stageDBO->drawTable();
-?>
-=======
+
+
 	$stageDBO = DatabaseObjectFactory::build('supplier');
 	$arr = ['company_name'];
 	//$stageDBO->UnicornMagic('gift_order', 'customer');
 	$records = $stageDBO->getRecords($arr);
 	$stageDBO->drawTable();
 ?>
-
->>>>>>> be9fcab15dc115e6abfdbd763cd736315d1cc18f
